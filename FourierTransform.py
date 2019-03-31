931ff339883760a6cabd2ee53470fe8b7b50f262
saikaveti <saileshkaveti@hotmail.com>
2019-03-31 02:45:49 -0400

    python stuff finally done

--- FourierTransform.py
+++ FourierTransform.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 #Beginning Example
 
@@ -18,5 +19,24 @@ plt.clf()
 
 plt.plot(signal_fft_pow, '-o', markersize = 2)
 plt.savefig('FFTResult1.png')
+
+plt.clf()
+
+
+#Number of Sample Points
+N = 10
+#Signal Work
+signal = np.r_[0 : math.pi * N : math.pi / 8.0]
+signal_sin = np.cos(signal) + np.sin(3 * signal)
+signal_fft = np.fft.fft(signal_sin)
+signal_fft_pow = np.abs(signal_fft)
+#Signal Plot Points
+plt.plot(signal_sin, '-o', markersize = 2)
+plt.savefig('FFTOriginal2.png')
+
+plt.clf()
+
+plt.plot(signal_fft_pow, '-o', markersize = 2)
+plt.savefig('FFTResult2.png')
 
 plt.clf()
